af0a05657809f8ba854a920f8438fe1642357281
Meoecle~ <william@ostengar.com>
2017-07-20 11:40:05 +0800

    hive

---
+++ src/main/java/HadoopUtils/HiveUtils.java
@@ -0,0 +1,131 @@
+package HadoopUtils;
+
+/**
+ * Created by Meowcle~ on 2017/7/19.
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import org.apache.log4j.Logger;
+
+/**
+ * Created by Meowcle~ on 2017/7/19.
+ */
+public class HiveUtils {
+
+    private static String driverName = "org.apache.hive.jdbc.HiveDriver";
+    private static String uri = "jdbc:hive2://localhost:10000/hive";
+    private static String user = "Meowcle~";
+    private static String password = "1234";
+    private static String sql = "";
+    private static ResultSet res;
+    private static final Logger log = Logger.getLogger(HiveUtils.class);
+    private static String tableName = "hivetesttable";
+
+    public static void countData(Statement stmt, String tableName)
+            throws SQLException {
+        sql = "select count(1) from " + tableName;
+        System.out.println("Running: " + sql);
+        res = stmt.executeQuery(sql);
+        System.out.println("Query result: ");
+        while (res.next()) {
+            System.out.println("count ------>" + res.getString(1));
+        }
+    }
+
+    public static void selectData(Statement stmt, String tableName)
+            throws SQLException {
+        sql = "select * from " + tableName;
+        System.out.println("Running: " + sql);
+        res = stmt.executeQuery(sql);
+        System.out.println("Query result: ");
+        while (res.next()) {
+            System.out.println(res.getInt(1) + "\t" + res.getString(2));
+        }
+    }
+
+    public static void describeTables(Statement stmt, String tableName)
+            throws SQLException {
+        sql = "describe " + tableName;
+        System.out.println("Running: " + sql);
+        res = stmt.executeQuery(sql);
+        System.out.println("Query result: ");
+        while (res.next()) {
+            System.out.println(res.getString(1) + "\t" + res.getString(2));
+        }
+    }
+
+    public static void showTables(Statement stmt, String tableName)
+            throws SQLException {
+        sql = "show tables '" + tableName + "'";
+        System.out.println("Running: " + sql);
+        res = stmt.executeQuery(sql);
+        System.out.println("Query result: ");
+        if (res.next()) {
+            System.out.println(res.getString(1));
+        }
+    }
+
+    public static void createTable(Statement stmt, String tableName)
+            throws SQLException {
+        sql = "create table " + tableName
+                + " (key int, value string)  row format delimited fields terminated by '\t'";
+        stmt.execute(sql);
+    }
+
+    public static boolean dropTable(Statement stmt, String tableName) throws SQLException {
+        // 创建的表名
+        sql = "drop table " + tableName;
+        stmt.execute(sql);
+        return true;
+    }
+
+    public static Connection getConn() throws ClassNotFoundException,
+            SQLException {
+        Class.forName(driverName);
+        Connection conn = DriverManager.getConnection(uri, user, password);
+        return conn;
+    }
+
+    public static void main(String[] args) {
+        Connection conn = null;
+        Statement stmt = null;
+        try {
+            conn = getConn();
+            stmt = conn.createStatement();
+
+            dropTable(stmt, tableName);
+            createTable(stmt, tableName);
+            showTables(stmt, tableName);
+            describeTables(stmt, tableName);
+            //loadData(stmt, tableName);
+            selectData(stmt, tableName);
+            countData(stmt, tableName);
+
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            log.error(driverName + " not found!", e);
+            System.exit(1);
+        } catch (SQLException e) {
+            e.printStackTrace();
+            log.error("Connection error!", e);
+            System.exit(1);
+        } finally {
+            try {
+                if (conn != null) {
+                    conn.close();
+                    conn = null;
+                }
+                if (stmt != null) {
+                    stmt.close();
+                    stmt = null;
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ src/test/java/HadoopUtils/HiveUtilsTest.java
@@ -0,0 +1,152 @@
+package HadoopUtils; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
+import static HadoopUtils.HiveUtils.getConn;
+import static org.junit.Assert.assertEquals;
+
+/** 
+* HiveUtils Tester. 
+* 
+* @author Meowcle~
+* @since <pre>07/19/2017</pre> 
+* @version 1.0 
+*/ 
+public class HiveUtilsTest {
+
+    Connection conn = null;
+    Statement stmt = null;
+    public String tableName = "hivetesttable";
+
+@Before
+public void before() throws Exception {
+    conn = getConn();
+    stmt = conn.createStatement();
+} 
+
+@After
+public void after() throws Exception { 
+}
+
+/** 
+* 
+* Method: countData(Statement stmt, String tableName) 
+* 
+*/ 
+@Test
+public void testCountData() throws Exception {
+    try{
+        HiveUtils.countData(stmt, tableName);
+    } catch(Exception ex){
+        ex.printStackTrace();
+        assertEquals(true, false);
+    }
+} 
+
+/** 
+* 
+* Method: selectData(Statement stmt, String tableName) 
+* 
+*/ 
+@Test
+public void testSelectData() throws Exception {
+    try{
+        HiveUtils.selectData(stmt, tableName);
+    } catch(Exception ex){
+        ex.printStackTrace();
+        assertEquals(true, false);
+    }
+} 
+
+/** 
+* 
+* Method: loadData(Statement stmt, String tableName) 
+* 
+*/ 
+@Test
+public void testLoadData() throws Exception {
+} 
+
+/** 
+* 
+* Method: describeTables(Statement stmt, String tableName) 
+* 
+*/ 
+@Test
+public void testDescribeTables() throws Exception {
+    try{
+        HiveUtils.describeTables(stmt, tableName);
+    } catch(Exception ex){
+        ex.printStackTrace();
+        assertEquals(true, false);
+    }
+} 
+
+/** 
+* 
+* Method: showTables(Statement stmt, String tableName) 
+* 
+*/ 
+@Test
+public void testShowTables() throws Exception {
+    try{
+        HiveUtils.showTables(stmt, tableName);
+    } catch(Exception ex){
+        ex.printStackTrace();
+        assertEquals(true, false);
+    }
+} 
+
+/** 
+* 
+* Method: createTable(Statement stmt, String tableName) 
+* 
+*/ 
+@Test
+public void testCreateTable() throws Exception {
+    try{
+        HiveUtils.createTable(stmt, tableName);
+    } catch(Exception ex){
+        ex.printStackTrace();
+        assertEquals(true, false);
+    }
+} 
+
+/** 
+* 
+* Method: dropTable(Statement stmt) 
+* 
+*/ 
+@Test
+public void testDropTable() throws Exception {
+    try{
+        assertEquals(true, HiveUtils.dropTable(stmt, tableName));
+    } catch(Exception ex){
+        ex.printStackTrace();
+        assertEquals(true, false);
+    }
+} 
+
+/** 
+* 
+* Method: getConn() 
+* 
+*/ 
+@Test
+public void testGetConn() throws Exception {
+}
+
+/**
+ *
+ * Method: main(String[] args)
+ *
+ */
+@Test
+public void testMain() throws Exception {
+}
+} 
